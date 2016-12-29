1c1273db8322ba006c2354a44d7437bf8e98f8d2
Scott Kaiser <scottkaiser87@gmail.com>
2016-12-29 11:20:04 -0500

    atlas template
    moved stewards into new ICPRB atlas template.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicmJvdXJhc3NhIiwiYSI6ImNpZ2NwOGM2MTN6Znh0ZW01Z2k5dnViMWsifQ.PDTaSyP-XHhBsvVRHrfx9w';
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2thaXNlcmljcHJiIiwiYSI6ImNpa2U3cGN1dDAwMnl1cm0yMW94bWNxbDEifQ.pEG_X7fqCAowSN8Xr6rX8g';
+
+var bound = new mapboxgl.LngLatBounds(
+    new mapboxgl.LngLat(-81.457, 36.945),
+	new mapboxgl.LngLat(-72.49, 41.17)
+);
 var map = new mapboxgl.Map({
 	container: 'map',
-	style: 'mapbox://styles/rbourassa/ciqns9rn10009bdmf44n8z2wl',
-	center: [-78.55, 39.00],
-	maxBounds: [
-	  [-90.0, 41.0],
-	  [-72.0, 35.0]
-	],
-	zoom: 6.7,
+	style: 'mapbox://styles/skaisericprb/citvqu6qb002p2jo1ig5hnvtk',
+	center: [-77.975, 39.077],
+	maxBounds: bound,
+	zoom: 7.45,
 	attributionControl: {
 		position: 'bottom-right'
-	}	
+	},
+	minZoom: [7.0],
 });
 
 var url = 'https://spreadsheets.google.com/feeds/list/1E-bTt31rGTDedtxbAh_mUeU7YZm0tClKkRnrwDLa06I/2/public/basic?alt=json';
@@ -19,37 +22,36 @@ var steward_template_string = "<% if (steward.name) { %><p><strong>Organization:
 var steward_template = _.template(steward_template_string, {variable: 'steward'});
 
 map.on('load', function() {
-
-  mapboxgl.util.getJSON(url, function(err, data) {
-	  document.body.classList.remove('loading');
-	  if (err) return console.warn(err);
-	  var geojson = {
-		  type: 'FeatureCollection',
-		  features: []
-	  };
 	
-	  data.feed.entry.forEach(function(d) {
-		  var fields = d.content.$t.split(', ');
-		  var lng = parseFloat(fields[5].split(': ')[1]);
-		  var lat = parseFloat(fields[4].split(': ')[1]);
-		  var contact = fields[1].split(': ')[1];
-		  var geo = fields[0].split(': ')[1];
+  mapboxgl.util.getJSON(url, function(err, data) {
+	document.body.classList.remove('loading');
+	if (err) return console.warn(err);
+	var geojson = {
+		type: 'FeatureCollection',
+		features: []
+	};
+	
+	data.feed.entry.forEach(function(d) {
+		var fields = d.content.$t.split(', ');
+		var lng = parseFloat(fields[5].split(': ')[1]);
+		var lat = parseFloat(fields[4].split(': ')[1]);
+		var contact = fields[1].split(': ')[1];
+		var geo = fields[0].split(': ')[1];
 		
-		  geojson.features.push({
-			  type: 'Feature',
-			  properties: {
-				  name: d.title.$t,
-				  contact: contact,
-				  geo: geo
-			  },
-			  geometry: {
-				  type: 'Point',
-				  coordinates: [lng, lat]
-			  }
-		  });
-	  });
-
-
+		geojson.features.push({
+			type: 'Feature',
+			properties: {
+				name: d.title.$t,
+				contact: contact,
+				geo: geo
+			},
+			geometry: {
+				type: 'Point',
+				coordinates: [lng, lat]
+			}
+		});
+    });	
+	
   map.addSource('data', {
 	  type: 'geojson',
 	  data: geojson
@@ -57,56 +59,43 @@ map.on('load', function() {
   
   map.addLayer({
 	  id: 'point',
-	  type: 'circle',
+	  type: 'symbol',
+	  layout: {
+		  'icon-image': 'marker-11',
+		  'icon-allow-overlap': false
+	  },
 	  source: 'data',
-	  paint: {
-		  'circle-radius': 6,
-		  'circle-color': "#4169DB"
-	  }
-    });
-	
-  map.on('click', function(e) {
-	  var features = map.queryRenderedFeatures(e.point, {
-		  layers: ['point']
-	  });
-	  
-	  if (!features.length) {
-		  return;
-	  }
-	  
-	  var feature = features[0];
-      var ttip = new mapboxgl.Popup()	  
-        .setLngLat(feature.geometry.coordinates)
-		.setHTML(steward_template(features[0].properties))
-		.addTo(map);	  
+	  paint: {}
   });
+});
+});
+
+
+map.on('click', function(e) {
+	var features = map.queryRenderedFeatures(e.point, {
+		layers: ['point']
+	});
+	  
+	if (!features.length) {
+		return;
+	}
+	  
+	var feature = features[0];
+    var ttip = new mapboxgl.Popup()	  
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML(steward_template(features[0].properties))
+	  .addTo(map);	  
+});
 
   
-  map.on('mousemove', function(e) {
-	  var features = map.queryRenderedFeatures(e.point, {
-		  layers: ['point']
-	  });
-  	  map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';	  
-  });
+map.on('mousemove', function(e) {
+	var features = map.queryRenderedFeatures(e.point, {
+		layers: ['point']
+	});
+  	map.getCanvas().style.cursor = (features.length) ? 'pointer' : '';	  
+});
 
-//  map.on('click', function(e) {
-//	  var features = map.queryRenderedFeatures(e.point, {
-//		  layers: ['HUC']
-//	  });
-//	  
-//	  if (!features.length) {
-//		  return;
-//	  }
-//	  
-//      var feature = features[0];
-//      var huctip = new mapboxgl.Popup()
-//	      .setLngLat(feature.geometry.coordinates)
-//		  .setHTML(features[0].properties.HUC_NAME)
-//		  .addTo(map);
-//	  });	  
-	  
-  map.addControl(new mapboxgl.Geocoder({position: 'top-right'}));
-  map.addControl(new mapboxgl.Navigation());
-  
-  });
-});  
+
+map.addControl(new mapboxgl.Geocoder({position: 'top-right'}));
+map.addControl(new mapboxgl.Navigation({position: 'top-left'}));
+
